f9e68fce54965a77c937ffa110fb85f991af6937
maoinbupt <maoinbupt@gmail.com>
2015-05-04 19:05:01 +0800

    addd one line from local

--- src/cn/bupt/hellogithub/MainActivity.java
+++ src/cn/bupt/hellogithub/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
         //Add one line from web
+		//Add one line from local
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
