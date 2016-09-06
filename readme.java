1e3e8550993db1bb7677e581904106c6c9088ae3
reamongao <reamongao@sohu-inc.com>
2016-09-06 19:26:17 +0800

    cherry-pick fix

--- readme.java
+++ readme.java
@@ -7,5 +7,11 @@ class A{
 	//git branch --set-upstream-to=origin/developer developer
 	// git push 
 	// this is  Added from web 
+
 	// added 201609061532
+
+	// added in bug_fix 201609031534
+	
+	// cherry pick test
+
 }
