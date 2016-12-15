704d6347098716ad88b32ea3d8d4a96c874e68e1
reamongao <reamongao@sohu-inc.com>
2016-12-15 19:11:58 +0800

    rebase test

--- readme.java
+++ readme.java
@@ -7,11 +7,9 @@ class A{
 	//git branch --set-upstream-to=origin/developer developer
 	// git push 
 	// this is  Added from web 
-
 	// added 201609061532
-
 	// added in bug_fix 201609031534
-	
 	// cherry pick test
+	// rebase test
 
 }
